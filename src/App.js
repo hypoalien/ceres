be319574e6731a9fd040fee84b523fd24527944b
hypoalien <hypoalien@gmail.com>
2020-09-20 13:56:34 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ForceGraph3D from 'react-force-graph-3d';
+import myData from './datasets/allneo.json';
+
+
 
 function App() {
+  
+  const { useRef, useCallback,useEffect  } = React;
+  const fgRef = useRef();
+  const handleClick = useCallback(node => {
+
+    // Aim at node from outside it
+    const distance = 40;
+    const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+
+    fgRef.current.cameraPosition(
+      { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+      node, // lookAt ({ x, y, z })
+      3000  // ms transition duration
+    );
+  }, [fgRef]);
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <ForceGraph3D
+        nodeLabel="html"
+        
+
+        ref={fgRef}
+        enableNodeDrag="false"
+        enablePointerInteraction="false"  
+       // onNodeClick={handleClick}
+        
+        graphData={myData}
+        //nodeVal="earthsize"
+        nodeAutoColorBy="group"
+  />
+  <div>TEAM HYPOALIEN</div>
     </div>
   );
 }
